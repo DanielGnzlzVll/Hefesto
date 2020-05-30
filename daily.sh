07b9db736eaeccd8c361e7afd474cd8d75db60fe
DanielGonzalez <jodgonzalezvi@unal.edu.co>
2020-05-30 17:24:59 -0500

    Test

---
+++ daily.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Reniciando el equipo."
+sudo reboot
---
+++ every_hour.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /home/pi/Hefesto
+git pull | grep "Already up to date."
+up_to_date_or_error=$?
+
+if [ $up_to_date_or_error -ne 0 ];
+then
+    echo "Reiniciando los contenedores"
+    docker-compose restart
+    echo "Reiniciando el equipo"
+    sudo reboot
+else
+    echo "Sin actualizaciones"
+fi;
